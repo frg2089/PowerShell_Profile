351c3b2426f5d52c8ea0d346ece92493a1f8765f
舰队的偶像-岛风酱! <frg2089@outlook.com>
2024-11-26 15:56:41 +0800

    update
    万恶之源 Microsoft.PowerShell.Core\FileSystem::
    
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- utils/PathForward.ps1
+++ utils/PathForward.ps1
@@ -1,7 +1,9 @@
 #Requires -Version 5.1
 
 function Set-LocationToParent {
-  Set-Location ..
+  $Private:ParentPath = Join-Path $PWD '..'
+  $Private:ParentPath = Convert-Path $Private:ParentPath
+  Set-Location $Private:ParentPath
 }
 
 Set-Alias -Name .. -Value Set-LocationToParent
