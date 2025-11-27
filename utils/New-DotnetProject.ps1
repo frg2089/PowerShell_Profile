59f775e5c11e9c87ce8e57ebdd4936a667764a14
舰队的偶像-岛风酱! <frg2089@outlook.com>
2025-11-27 18:56:50 +0800

    快速添加项目到解决方案
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- utils/New-DotnetProject.ps1
+++ utils/New-DotnetProject.ps1
@@ -1,5 +1,49 @@
 #Requires -Version 7.4
 
+function Add-DotnetProjects {
+  [CmdletBinding()]
+  param (
+    [string]
+    $Solution,
+    [string]
+    $ProjectsDirectory,
+    [switch]
+    $NoRoot
+  )
+  $Private:CommandArgs = '--in-root'
+  if ($NoRoot) {
+    $Private:CommandArgs = ''
+  }
+
+  function Find-Solution ([string]$Path) {
+    $Solution = Get-ChildItem -Path $Path -Filter '*.slnx' | Select-Object -First 1
+    if (!$Solution) {
+      $Solution = Get-ChildItem -Path $Path -Filter '*.sln' | Select-Object -First 1
+    }
+
+    return $Solution
+  }
+  if (!$Solution) {
+    $Solution = Find-Solution -Path (Get-Location)
+    if (!$Solution) {
+      $Solution = Find-Solution -Path (Split-Path -Path (Get-Location))
+    }
+  }
+
+  $Private:SolutionDirectory = Split-Path -Path $Solution
+
+  if (!$ProjectsDirectory) {
+    $ProjectsDirectory = Get-ChildItem -Directory -Path $Private:SolutionDirectory | Where-Object Name -EQ 'src' | Select-Object -First 1
+    if (!$ProjectsDirectory) {
+      $ProjectsDirectory = $Private:SolutionDirectory
+    }
+  }
+
+  Get-ChildItem -Path $ProjectsDirectory | ForEach-Object { 
+    dotnet sln $Solution add $_.FullName $Private:CommandArgs
+  }
+}
+
 function New-DotnetProject {
   [CmdletBinding()]
   param (
